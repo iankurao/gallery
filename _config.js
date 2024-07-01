18b6ecdccadb3be5b2341078cf807a57a09abaaa
Ian  K <47345812+iankurao@users.noreply.github.com>
2024-07-01 15:21:38 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://iankurao:valley54321@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    development: 'mongodb+srv://iankurao:valley54321>@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://iankurao:valley54321@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     test: 'mongodb+srv://iankurao:valley54321@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
