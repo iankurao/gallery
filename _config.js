948869348c1a761d85773a7136b750d74ddef0ad
Ian  K <47345812+iankurao@users.noreply.github.com>
2024-07-05 17:53:01 +0300

    Npm test 1

--- _config.js
+++ _config.js
@@ -1,10 +1,9 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://iankurao:valley54321@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    development: 'mongodb+srv://iankurao:valley54321@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    test: 'mongodb+srv://iankurao:valley54321@cluster0.peqbmwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  production: 'mongodb+srv://yourUsername:yourPassword@cluster0.mongodb.net/darkroom?retryWrites=true&w=majority',
+  development: 'mongodb+srv://yourUsername:yourPassword@cluster0.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  test: 'mongodb+srv://yourUsername:yourPassword@cluster0.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
-module.exports = config;
 
+module.exports = config;
