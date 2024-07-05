5c27afa6550d075b802e947f5040d82cb7eac6cf
Ian  K <47345812+iankurao@users.noreply.github.com>
2024-07-05 18:01:55 +0300

    Update and rename views/test connection.js to ian.ejs

--- views/test connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const db = 'mongodb+srv://iankurao:valley54321@cluster0.mongodb.net/darkroom?retryWrites=true&w=majority';
-
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
