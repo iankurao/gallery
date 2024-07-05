a21b1fa69660bc11e75aa498c73b65adba822510
Ian  K <47345812+iankurao@users.noreply.github.com>
2024-07-05 17:56:15 +0300

    Npm test 2- verify mongodb connection

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
 var config = {}
 
 config.mongoURI = {
-  production: 'mongodb+srv://yourUsername:yourPassword@cluster0.mongodb.net/darkroom?retryWrites=true&w=majority',
-  development: 'mongodb+srv://yourUsername:yourPassword@cluster0.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-  test: 'mongodb+srv://yourUsername:yourPassword@cluster0.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+  production: 'mongodb+srv://iankurao:valley54321@cluster0.mongodb.net/darkroom?retryWrites=true&w=majority',
+  development: 'mongodb+srv://iankurao:valley54321@cluster0.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+  test: 'mongodb+srv://iankurao:valley54321@cluster0.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 
 module.exports = config;
